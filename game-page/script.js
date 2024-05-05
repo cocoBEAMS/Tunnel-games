5b2aa9411036bfe13ee64375db469d733acba65b
CoolBeans <147670938+cocoBEAMS@users.noreply.github.com>
2024-05-05 18:07:54 +0000

    added Discord tag.
    yay

--- game-page/script.js
+++
@@ -1,7 +0,0 @@
-const imageContainers = document.querySelectorAll('.image-container');
-
-imageContainers.forEach(container => {
-  container.addEventListener('mouseenter', () => {
-    container.querySelector('.image-title').style.opacity = 1;
-  });
-});
